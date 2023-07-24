6e6a645c2c39b13bf5f079e61b9aee9fd3cb7307
KenStarry <starrycodes@gmail.com>
2023-07-24 11:00:57 +0300

    Launch Url in browser

--- launch_urls_flutter/lib/main_screen.dart
+++ launch_urls_flutter/lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:launch_urls_flutter/url_intents.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -12,7 +13,14 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(child: Text("Hi")),
+      body: SafeArea(
+          child: Column(
+        children: [
+          FilledButton(
+              onPressed: () async => await launchInBrowser(),
+              child: const Text("Launch in Browser"))
+        ],
+      )),
     );
   }
 }
