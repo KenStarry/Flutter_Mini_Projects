051cb0f52c263682288579bafd73fc1a8db2bb04
KenStarry <starrycodes@gmail.com>
2023-07-26 16:04:22 +0300

    Finished Rest APIs

--- rest_apis/lib/home_page.dart
+++ rest_apis/lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rest_apis/post.dart';
+import 'package:rest_apis/remote_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,15 +21,25 @@ class _HomePageState extends State<HomePage> {
   }
 
   getData() async {
+    posts = await RemoteService().getPosts();
 
+    if (posts != null) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) => Container(child: const Text("Hi")),
-        itemCount: 10,
+      body: Visibility(
+        visible: isLoaded,
+        child: ListView.builder(
+          itemBuilder: (context, index) => Container(child: Text(posts?[index].title ?? "")),
+          itemCount: posts?.length ?? 0,
+        ),
+        replacement: CircularProgressIndicator(),
       ),
     );
   }
