064feb283fe66bce639ef620a2e71dbed0dff1f6
KenStarry <starrycodes@gmail.com>
2023-07-26 15:58:21 +0300

    Added Remote Service to fetch our data

--- rest_apis/lib/post.dart
+++ rest_apis/lib/post.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-List<Post> postFromJson(String str) => List<Post>.from(json.decode(str).map((x) => Post.fromJson(x)));
+List<Post> postsFromJson(String url) => List<Post>.from(json.decode(url).map((x) => Post.fromJson(x)));
 
 String postToJson(List<Post> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
--- rest_apis/lib/remote_service.dart
+++ rest_apis/lib/remote_service.dart
@@ -1,7 +1,19 @@
 import 'package:rest_apis/post.dart';
+import 'package:http/http.dart' as http;
 
 class RemoteService {
   Future<List<Post>?> getPosts() async {
 
+    var client = http.Client();
+    var uri = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+
+    var response = await client.get(uri);
+
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postsFromJson(json);
+    }
+
+    return null;
   }
 }
