5fddbf75b8d7aca1f8fd32dc98afde511cae31e7
KenStarry <starrycodes@gmail.com>
2023-07-28 12:13:41 +0300

    Added My TextField

---
+++ chatty_flutter/lib/pages/login_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              //  logo
+              Icon(Icons.message_rounded, size: 80,),
+
+              Text("Welcome back you've been missed", style: TextStyle(
+                fontSize: 16
+              ),)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
