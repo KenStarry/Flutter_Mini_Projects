cd370fbdc3878109b0bbba430a3e247af6162624
KenStarry <starrycodes@gmail.com>
2023-07-28 13:01:32 +0300

    Added Firebase Auth Service

---
+++ chatty_flutter/lib/services/auth/auth_service.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  //  sign in
+  Future<UserCredential> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      return userCredential;
+
+    } on FirebaseAuthException catch (error) {
+      throw Exception(error.code);
+    }
+  }
+
+//  sign out
+}
