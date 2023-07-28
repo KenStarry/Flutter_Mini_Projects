80a1ad2647ba56e8eb262c4ed52b0a8e7e44b626
KenStarry <starrycodes@gmail.com>
2023-07-28 16:42:32 +0300

    Added Functionality to send a message

--- chatty_flutter/lib/pages/chat_page.dart
+++ chatty_flutter/lib/pages/chat_page.dart
@@ -1,16 +1,32 @@
+import 'package:chatty_flutter/services/chat_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
 
   final String receiverUserID;
 
-  const ChatPage({super.key,required this.receiverUserID});
+  const ChatPage({super.key, required this.receiverUserID});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
+
+  final TextEditingController _messageController = TextEditingController();
+  final ChatService _chatService = ChatService();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  void sendMessage() async {
+    if (_messageController.text.isNotEmpty) {
+      await _chatService.sendMessage(
+          receiverID: widget.receiverUserID, message: _messageController.text);
+
+      _messageController.clear();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
