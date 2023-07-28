372aaf63de13fb5481641fef97050929d450b65f
KenStarry <starrycodes@gmail.com>
2023-07-28 12:25:34 +0300

    Added My Button

---
+++ chatty_flutter/lib/components/my_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final void Function()? onTap;
+
+  const MyButton({super.key, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+        ),
+        child: Center(
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+}
