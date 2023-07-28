7ca32e4c1d872a02c80089f5de445aa73b013b48
KenStarry <starrycodes@gmail.com>
2023-07-28 13:28:26 +0300

    Added Sign Out Functionality

--- chatty_flutter/lib/pages/home_page.dart
+++ chatty_flutter/lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:chatty_flutter/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,9 +10,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  void signOut() async {
+    final authService = Provider.of<AuthService>(context, listen: false);
+
+    authService.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+        actions: [
+          IconButton(onPressed: signOut, icon: const Icon(Icons.logout))
+        ],
+      ),
       body: Text("Welcome Home"),
     );
   }
