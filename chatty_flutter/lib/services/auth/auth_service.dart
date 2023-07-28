7e32ee2c8f028746e91abcd4ac78419bfc810688
KenStarry <starrycodes@gmail.com>
2023-07-28 15:04:01 +0300

    Added Firestore dependency

--- chatty_flutter/lib/services/auth/auth_service.dart
+++ chatty_flutter/lib/services/auth/auth_service.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 
 class AuthService extends ChangeNotifier{
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   //  sign in
   Future<UserCredential> signInWithEmailAndPassword(
@@ -24,6 +26,8 @@ class AuthService extends ChangeNotifier{
     try {
       UserCredential userCredential = await _firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password);
+
+      //  create a new user document
 
       return userCredential;
 
